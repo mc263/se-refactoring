bec3e6afadad43e5d2e1aed0535c2452a1b04463
mc263 <busam.natalie@student.dhbw-karlsruhe.de>
2019-05-06 21:14:31 +0200

    Tests for Customer-Class

--- src/CustomerTest.java
+++ src/CustomerTest.java
@@ -12,7 +12,7 @@ public class CustomerTest {
 		Rental rental = new Rental(new Movie("beverly hills cop", 20), 2);
 		customer.addRental(rental);
 
-		Vector vector = customer.getRentals();
+		Vector<Rental> vector = customer.getRentals();
 
 		Assert.assertEquals(rental, vector.get(0));
 	}
@@ -25,17 +25,41 @@ public class CustomerTest {
 		Rental rental2 = new Rental(new Movie("saw2", 20), 2);
 		customer.addRental(rental2);
 
-		Vector vector = customer.getRentals();
+		Vector<Rental> vector = customer.getRentals();
 
 		Assert.assertNotEquals(rental2, vector.get(0));
 	}
 
 	@Test
-	public void foo() {
-		int a = 1;
-		int b = 2;
+	public void testGetName() {
+		Assert.assertEquals("kai", customer.getName());
+	}
 
-		Assert.assertEquals(4, a + b);
+	@Test
+	public void testGetRentals() {
+
+		Rental rental = new Rental(new Movie("beverly hills cop", 20), 2);
+		customer.addRental(rental);
+
+		Vector<Rental> vector = customer.getRentals();
+
+		Assert.assertEquals(vector, customer.getRentals());
+
+	}
+
+	@Test
+	public void testStatement() throws Exception {
+		Movie m1 = new Movie("movie1", 1);
+		Movie m2 = new Movie("movie2", 2);
+		Rental r1 = new Rental(m1, 10);
+		Rental r2 = new Rental(m2, 5);
+		customer.addRental(r1);
+		customer.addRental(r2);
+		String result = customer.statement();
+		Assert.assertEquals(
+				"Rental Record for kai\n" + "\tTitle\t\tDays\tAmount\n" + "\tmovie1\t\t10\t30.0\n"
+						+ "\tmovie2\t\t5\t4.5\n" + "Amount owed is 34.5\n" + "You earned 3 frequent renter points",
+				result);
 	}
 
 }
